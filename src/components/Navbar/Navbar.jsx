1765c9cfb857813dad4350aebe893f34cbb5411d
Reshav <thapareshav50@gmail.com>
2022-12-10 23:26:57 +0545

    Index file modified

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+const Navbar = () => {
+  return (
+    <>
+      <ul className="nav justify-content-end">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Active</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+    </>
+  )
+}
+
+export default navbar
