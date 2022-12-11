c4cf2a7bf71c64f63f440e57be803a202aba1db8
Reshav <thapareshav50@gmail.com>
2022-12-11 18:25:22 +0545

    Fixed Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import Navbar from "../src/components/Navbar/Navbar"
+import Navbar from "./components/Navbar/Navbar"
 
 const App = () => {
   return (
-    <>
+    
       <Navbar />
-    </>
+    
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 import App from './App';
 
 
-ReactDOM.render(
-    <App />, 
-    document.getElementById('root')
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+    <App />
 );
