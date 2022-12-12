fe6b0c155509f7e06c1d18a3f35bd35a2ad7dd72
Reshav <thapareshav50@gmail.com>
2022-12-12 22:32:36 +0545

    css for background image left

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import architecture from './architecture.jpg'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <h1>
+        Best architecture in Nepal.
+      </h1>
+      <h2>Team of talented people.</h2>
+      <div>
+        <img src = {architecture} alt = "architecture.jpg" />
+      </div>
+    </div>
   )
 }
 
