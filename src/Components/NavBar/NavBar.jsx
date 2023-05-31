a8c3f5607ce2c893dd3d80dadb7f7844420d4131
Reshav <thapareshav50@gmail.com>
2023-05-31 07:44:15 +0545

    .

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,19 +1,58 @@
-import './NavBar.css'
+import React from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
 
+import './NavBar.css';
 
-const NavBar = () =>
-{
-    return(
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = React.useState(false);
 
-        <nav className = "navbar">
-            <a href="/">Home</a>
-            <a href="/aboutus">About Us</a>
-            <a href="/projects">Projects</a>
-            <a href="/Contact Us">Contact Us</a>
-        </nav>
-       
+  return (
+    <div className="navbar">
+      <GiHamburgerMenu
+        className="hamburger-icon"
+        color="#fff"
+        fontSize={27}
+        onClick={() => setToggleMenu(true)}
+      />
+      {toggleMenu && (
+        <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+          <MdOutlineRestaurantMenu
+            fontSize={27}
+            className="overlay__close"
+            onClick={() => setToggleMenu(false)}
+          />
+          <ul className="app__navbar-smallscreen_links">
+            <li>
+              <a href="#home" onClick={() => setToggleMenu(false)}>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#about" onClick={() => setToggleMenu(false)}>
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#menu" onClick={() => setToggleMenu(false)}>
+                Menu
+              </a>
+            </li>
+            <li>
+              <a href="#awards" onClick={() => setToggleMenu(false)}>
+                Awards
+              </a>
+            </li>
+            <li>
+              <a href="#contact" onClick={() => setToggleMenu(false)}>
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
 
-    )
-}
-
-export default NavBar
+export default Navbar;
